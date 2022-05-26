9caa4aabcfe9a4a5f0f5e98c5138d92f059f8782
nisaefendioglu <nisaefendioglu0@gmail.com>
2022-05-26 21:16:28 +0300

    fix

--- app/src/main/java/com/nisaefendioglu/daggerhiltinandroidwithexample/MainActivity.kt
+++ app/src/main/java/com/nisaefendioglu/daggerhiltinandroidwithexample/MainActivity.kt
@@ -2,10 +2,32 @@ package com.nisaefendioglu.daggerhiltinandroidwithexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.nisaefendioglu.daggerhiltinandroidwithexample.adapter.CryptocurrencyAdapter
+import com.nisaefendioglu.daggerhiltinandroidwithexample.viewmodel.MainViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    // viewModels() delegate used to get
+    // ViewModel in MainActivity class
+    private val viewModel: MainViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        observeCryptoCurrency()
+    }
+
+    // Observing the live data
+    private fun observeCryptoCurrency() {
+        viewModel.cryptoCurrency.observe(this) {
+            viewModel.cryptoCurrency.observe(this) {
+                val cryptocurrencyList = findViewById<RecyclerView>(R.id.cryptocurrency_list)
+                cryptocurrencyList.layoutManager = LinearLayoutManager(this)
+                cryptocurrencyList.adapter = CryptocurrencyAdapter(it)
+            }
+        }
     }
 }
